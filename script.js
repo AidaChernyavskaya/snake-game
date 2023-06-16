a6a91f9090f162eab3762c84597f768cfca4ad16
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2023-06-16 18:07:26 +0300

    snake can be controlled using keyboard

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const canvas = document.getElementById('game');
 const context = canvas.getContext('2d');
 let speed = 7;
 let tileCount = 30;
-let tileSize = canvas.clientWidth/tileCount - 2;
+let tileSize = canvas.clientWidth/tileCount - 1;
+let xDirection = 0;
+let yDirection = 0;
 
 let headX = 15;
 let headY = 15;
@@ -10,6 +12,7 @@ let headY = 15;
 const drawGame = () => {
     clearScreen();
     drawSnake();
+    changeSnakePosition();
 
     setTimeout(drawGame, 1000/speed);
 }
@@ -24,4 +27,29 @@ const clearScreen = () => {
     context.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight)
 }
 
+const changeSnakePosition = () => {
+    headX += xDirection;
+    headY += yDirection;
+}
+
+const keyDown = (event) => {
+    if (event.key === 'ArrowUp' && yDirection !== 1) {
+        xDirection = 0;
+        yDirection = -1;
+    }
+    if (event.key === 'ArrowRight' && xDirection !== -1) {
+        xDirection = 1;
+        yDirection = 0;
+    }
+    if (event.key === 'ArrowDown' && yDirection !== -1) {
+        xDirection = 0;
+        yDirection = 1;
+    }
+    if (event.key === 'ArrowLeft' && xDirection !== 1) {
+        xDirection = -1;
+        yDirection = 0;
+    }
+}
+
 drawGame();
+document.body.addEventListener('keydown', keyDown);
