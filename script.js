757d0976db4f0e89ba58b840f39a54960590a8ca
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2023-06-16 13:46:53 +0300

    snake is rendered

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 const canvas = document.getElementById('game');
 const context = canvas.getContext('2d');
+let speed = 7;
+let tileCount = 30;
+let tileSize = canvas.clientWidth/tileCount - 2;
+
+let headX = 15;
+let headY = 15;
+
+const drawGame = () => {
+    clearScreen();
+    drawSnake();
+
+    setTimeout(drawGame, 1000/speed);
+}
+
+const drawSnake = () => {
+    context.fillStyle = 'midnightblue';
+    context.fillRect(headX * tileSize, headY * tileSize, tileCount, tileCount);
+}
+
+const clearScreen = () => {
+    context.fillStyle = 'palegreen';
+    context.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight)
+}
+
+drawGame();
