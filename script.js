f04c67bd13e29fed816b86123e5e20e3a7da52c5
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2023-06-17 15:33:38 +0300

    snake grows if it eats a food

--- script.js
+++ script.js
@@ -1,33 +1,56 @@
 const canvas = document.getElementById('game');
 const context = canvas.getContext('2d');
-let speed = 7;
+let speed = 6;
 let tileCount = 30;
-let tileSize = canvas.clientWidth/tileCount - 1;
+let tileSize = canvas.clientWidth/tileCount + 8;
 let xDirection = 0;
 let yDirection = 0;
 
-let headX = 15;
-let headY = 15;
+let headX = 10;
+let headY = 10;
 let foodX = 5;
 let foodY = 5;
 
+const snakeParts = [];
+let tailLength = 2;
+
+class snakePart {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
 const drawGame = () => {
     clearScreen();
+
     drawSnake();
     drawFood();
+
     changeSnakePosition();
+    checkCollision(tileCount - 10);
 
     setTimeout(drawGame, 1000/speed);
 }
 
 const drawSnake = () => {
+    context.fillStyle = 'navy';
+    for (let i = 0; i < snakeParts.length; i++){
+        let part = snakeParts[i];
+        context.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize);
+    }
+    snakeParts.push(new snakePart(headX, headY));
+    if (snakeParts.length > tailLength){
+        snakeParts.shift();
+    }
+
     context.fillStyle = 'midnightblue';
-    context.fillRect(headX * tileSize, headY * tileSize, tileCount, tileCount);
+    context.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
 }
 
 const drawFood = () => {
     context.fillStyle = 'red';
-    context.fillRect(foodX * tileSize, foodY * tileSize, tileCount, tileCount);
+    context.fillRect(foodX * tileCount, foodY * tileCount, tileSize, tileSize);
 }
 
 const clearScreen = () => {
@@ -38,6 +61,14 @@ const clearScreen = () => {
 const changeSnakePosition = () => {
     headX += xDirection;
     headY += yDirection;
+}
+
+const checkCollision = (max) => {
+    if (foodX === headX && foodY === headY){
+        foodX = Math.floor(Math.random() * max);
+        foodY = Math.floor(Math.random() * max);
+        tailLength++;
+    }
 }
 
 const keyDown = (event) => {
